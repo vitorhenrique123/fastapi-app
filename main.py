4ba7d65255068df6231fe342347ba5382cfda5bb
Vitor Leal <vitor@vitorhenrique.me>
2026-07-13 03:24:04 -0300

    test

--- main.py
+++ main.py
@@ -8,7 +8,15 @@ class ValidateRequest(BaseModel):
     token: str
     hwid: str
 
+class ValidateRequest2(BaseModel):
+    license_key: str
+    hwid: str
 
 @app.post("/apii/auth/validate")
 def validate(request: ValidateRequest):
     return {}
+
+@app.post("/apii/auth/login")
+def validate2(request: ValidateRequest2):
+    return {}
+
